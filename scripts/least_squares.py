cd7aecc7172d931651a4b0267eb029148eece509
Dimitrios Sarigiannis <saridimi@gmail.com>
2016-10-20 15:17:19 +0200

    [UPDATE] LS

--- scripts/least_squares.py
+++ scripts/least_squares.py
@@ -12,9 +12,7 @@ def least_squares(y, tx):
     """calculate the least squares solution."""
     # least squares: TODO
     # returns mse, and optimal weights
-    
-    # problem with linalg.inv
-#     w_opt = np.linalg.inv(np.transpose(tx).dot(tx)).dot(np.transpose(tx)).dot(y)
+        
     w_opt = np.linalg.solve(np.transpose(tx).dot(tx), np.transpose(tx).dot(y))
     mse = compute_loss(y, tx, w_opt)
     
