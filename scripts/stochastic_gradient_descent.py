de481b5a2293165645b8b7ca2963aa99718984d6
Dimitrios Sarigiannis <saridimi@gmail.com>
2016-10-20 18:15:26 +0200

    [UPDATE] Stochastic GD

--- scripts/stochastic_gradient_descent.py
+++ scripts/stochastic_gradient_descent.py
@@ -34,7 +34,8 @@ def stochastic_gradient_descent(y, tx, initial_w, batch_size, max_iters, gamma):
 
         ws.append(np.copy(w))
         losses.append(loss)
-        print("Stochastic Gradient Descent({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format(
-                bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
+        print("Stochastic Gradient Descent({bi}/{ti}): loss={l}".format(bi=n_iter, ti=max_iters - 1, l=loss))
+
+    print('parameters w: ',ws[-1])
 
     return losses, ws
