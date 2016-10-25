41546cd444e40abc7b9a1c99bfbd581742b626cd
Dimitrios Sarigiannis <saridimi@gmail.com>
2016-10-25 14:42:26 +0200

    [UPDATE] test.py

--- scripts/test.py
+++ scripts/test.py
@@ -11,53 +11,55 @@ def score(tX, y, w):
 	return (labels == y).sum()/labels.size
 
 
-def test_GD(y,tX,ratio,max_iters=1000,gamma=0.01,w_initial=None):
+# Gradient descent
+def test_GD(y,tX,ratio,gamma=0.01,max_iters=1000,w_initial=None):
 	seed = 1
 	tX_tr, y_tr, tX_te, y_te = split_data(y, tX, ratio, seed)
 
-	# Gradient descent
 	if w_initial == None:
 		w_initial = np.zeros(tX_tr.shape[1])
-	_, w = least_squares_GD(y_tr, tX_tr, w_initial, max_iters, gamma)
+
+	_, w = least_squares_GD(y_tr, tX_tr, w_initial,gamma,max_iters)
 	s = score(tX_te, y_te, w)
 	print(s)
 
-def test_SGD(y,tX,ratio,max_iters,gamma,batch_size,w_initial=None):
+# Stochastic gradient descent
+def test_SGD(y,tX,ratio,gamma=0.01,max_iters=1000,batch_size=50,w_initial=None):
 	seed = 1
 	tX_tr, y_tr, tX_te, y_te = split_data(y, tX, ratio, seed)
 
-	# Stochastic gradient descent
 	if w_initial == None:
 		w_initial = np.zeros(tX_tr.shape[1])
-	_, w = least_squares_SGD(y_tr, tX_tr, w_initial, batch_size, max_iters, gamma)
+
+	_, w = least_squares_SGD(y_tr, tX_tr, w_initial, batch_size,gamma,max_iters)
 	s = score(tX_te, y_te, w)
 	print(s)
 
+# Least squares
 def test_LS(y,tX,ratio):
 	seed = 1
 	tX_tr, y_tr, tX_te, y_te = split_data(y, tX, ratio, seed)
 
-	# Least squares
 	w, _ = least_squares(y_tr, tX_tr)
 	s = score(tX_te, y_te, w)
 	print(s)
 
+# Ridge regression
 def test_RR(y, tX, ratio):
 	seed = 1
 	tX_tr, y_tr, tX_te, y_te = split_data(y, tX, ratio, seed)
-	# Ridge regression
 	w = ridge_regression(y_tr, tX_tr, 0.01)
 	s = score(tX_te, y_te, w)
 	print(s)
 
+# Logistic regression
 def test_LR(y, tX, ratio):
 	seed = 1
 	tX_tr, y_tr, tX_te, y_te = split_data(y, tX, ratio, seed)
-	# Logistic regression
 	print('not implemented')
 
+# Regularized logistic regression
 def test_RLR(y, tX, ratio):
 	seed = 1
 	tX_tr, y_tr, tX_te, y_te = split_data(y, tX, ratio, seed)
-	# Regularized logistic regression
 	print('not implemented')
