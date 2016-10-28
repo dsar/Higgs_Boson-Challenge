821bd778eaa8e778337e8e488c1b118b6b618da7
Loic Ottet <loic.ottet@epfl.ch>
2016-10-28 13:22:24 +0200

    Updates

--- scripts/test.py
+++ scripts/test.py
@@ -1,6 +1,6 @@
 import numpy as np
 from split_data import split_data
-from proj1_helpers import predict_labels
+from proj1_helpers import *
 from helpers import get_min_param_index
 from least_squares import least_squares
 from gradient_descent import least_squares_GD
@@ -45,17 +45,19 @@ def test_SGD(y, tX, ratio=0.5, w_initial=None, gamma=0.01, max_iters=1000, batch
 	min_index, _ = get_min_param_index(losses)
 
 	s = score(x_test, y_test, w[min_index])
-	
+	return s
+
 	print('SGD score: ',s)
 	print()
 
 # Least squares
 def test_LS(y,tX,ratio=0.5,seed=1):
-	print('LS test')
+	#print('LS test')
 	y_test, y_train, x_test, x_train  = split_data(y, tX, ratio, seed)
 
 	w, _ = least_squares(y_train, x_train)
 	s = score(x_test, y_test, w)
+	return s
 	print("LS score: ",s)
 	print()
 
@@ -65,6 +67,7 @@ def test_RR(y, tX, ratio=0.5,lambda_=0.01,seed = 1):
 	y_test, y_train, x_test, x_train  = split_data(y, tX, ratio, seed)
 	w = ridge_regression(y_train, x_train, lambda_)
 	s = score(x_test, y_test, w)
+	return s
 	print('Ridge Regression score: ',s)
 	print()
 
@@ -75,11 +78,13 @@ def test_LR(y, tX, ratio=0.5, seed=1, print_=False):
 
 	loss, w = logistic_regression_gradient_descent(y, tX,print_=False)
 	s = score(x_test,y_test,w)
+	return s
 	print('Logistic Regression score: ',s)
 	print()
 
 # Regularized logistic regression
 def test_RLR(y, tX, ratio=0.5,seed = 1):
 	y_test, y_train, x_test, x_train  = split_data(y, tX, ratio, seed)
+	return 0
 	print('not implemented yet')
 	print()
